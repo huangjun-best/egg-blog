116faa94df5bc2aeeccef7adca32a6da2a951b99
huangjun-best <huangjun_best@sina.com>
2015-06-02 17:01:30 +0800

    less

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var http = require("http");
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -20,10 +19,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 
-app.use('/', routes);
-app.use('/users', users);
+routes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +53,87 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+app.listen(port, function() {
+    console.log("server is start listen:" + port);
+});
+//var debug = require('debug')('bootstrap:server');
+//
+//var port = normalizePort(process.env.PORT || '3000');
+//app.set('port', port);
+//
+///**
+// * Create HTTP server.
+// */
+//
+//var server = http.createServer(app);
+//
+///**
+// * Listen on provided port, on all network interfaces.
+// */
+//
+//server.listen(port);
+//server.on('error', onError);
+//server.on('listening', onListening);
+//
+//function onError(error) {
+//  if (error.syscall !== 'listen') {
+//    throw error;
+//  }
+//
+//  var bind = typeof port === 'string'
+//    ? 'Pipe ' + port
+//    : 'Port ' + port;
+//
+//  // handle specific listen errors with friendly messages
+//  switch (error.code) {
+//    case 'EACCES':
+//      console.error(bind + ' requires elevated privileges');
+//      process.exit(1);
+//      break;
+//    case 'EADDRINUSE':
+//      console.error(bind + ' is already in use');
+//      process.exit(1);
+//      break;
+//    default:
+//      throw error;
+//  }
+//}
+//
+///**
+// * Event listener for HTTP server "listening" event.
+// */
+//
+//function onListening() {
+//  var addr = server.address();
+//  var bind = typeof addr === 'string'
+//    ? 'pipe ' + addr
+//    : 'port ' + addr.port;
+//  debug('Listening on ' + bind);
+//}
 
 
 module.exports = app;
